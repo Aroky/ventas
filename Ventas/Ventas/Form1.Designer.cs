73b11c7a285db57ab84849e4ad910b67615ecc81
Aroky Cruz <aroky07@gmail.com>
2021-06-06 11:04:15 -0600

    Agregando frase motivadora

--- Ventas/Ventas/Form1.Designer.cs
+++ Ventas/Ventas/Form1.Designer.cs
@@ -33,9 +33,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(100, 162);
+            this.button1.Location = new System.Drawing.Point(72, 153);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(139, 51);
             this.button1.TabIndex = 0;
             this.button1.Text = "Procesar";
             this.button1.UseVisualStyleBackColor = true;
